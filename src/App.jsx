e1a8123f5a360860dd3017caf0f6bf507919aeb6
jayt-piggie <jayt56834@gmail.com>
2025-07-06 22:46:42 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "1019096417355",
   appId: "1:1019096417355:web:e7f0211bbfd08d556679ce"
 };
-const appId = 'trip-itinerary-viewer';
 
 const parseMarkdown = (text) => {
     if (window.marked) {
